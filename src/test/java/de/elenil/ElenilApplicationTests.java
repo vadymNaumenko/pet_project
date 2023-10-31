9d58612fde65275f6b8f0e9aa541cce0ff487cac
VdymHunter <vadimnaumnaum@gmail.com>
2023-10-31 22:26:17 +0100

    first commit

---
+++ src/test/java/de/elenil/ElenilApplicationTests.java
@@ -0,0 +1,13 @@
+package de.elenil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElenilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
