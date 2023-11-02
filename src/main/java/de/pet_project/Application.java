90daad309b7d7d66786645867eb044aff9aa2876
unknown <semendiaievaviktoriia@gmail.com>
2023-11-02 14:04:24 +0100

    start(corrected_Application)

--- src/main/java/de/pet_project/Application.java
+++ src/main/java/de/pet_project/Application.java
@@ -1,5 +1,13 @@
 package de.pet_project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
