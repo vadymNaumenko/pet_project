695441597488e1898118275b86a8f4a73fddc475
Vdym <vadimnaumnaum@gmail.com>
2024-01-06 12:59:29 +0100

    added games post

--- src/main/java/de/pet_project/dto/event/EventCreateDTO.java
+++ src/main/java/de/pet_project/dto/event/EventCreateDTO.java
@@ -10,14 +10,8 @@ import java.time.LocalDateTime;
 public class EventCreateDTO {
     private String title;
     private String imageUrl;
+    private String videoUrl;
     private String text;
-    private String urlToNews;
     private String dateTime;
 
-    public EventCreateDTO(String title, String imageUrl, String urlToNews, String dateTime) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.urlToNews = urlToNews;
-        this.dateTime = dateTime;
-    }
 }
--- src/main/java/de/pet_project/service/EventService.java
+++ src/main/java/de/pet_project/service/EventService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,34 +22,66 @@ public class EventService {
 
     private final EventRepository eventRepository;
     private boolean enable = true; // todo mast be add in application.yml
-    private String sitUrl = "https://www.uploadvr.com/reviews";
-//    DEFAULT  'https://www.uploadvr.com'
+    private String newsPage = "https://www.uploadvr.com/reviews";
+    private String site = "https://www.uploadvr.com";
 
 
     //    private List<EventCreateDTO> readTitle(){
     @Scheduled(fixedRate = 2 * 60 * 1000)
-    private void readTitle() {
+    private void readNews() {
         ArrayList<EventCreateDTO> events = new ArrayList<>();
-
         try {
-            Document document = Jsoup.connect(sitUrl).get();
+            Document document = Jsoup.connect(newsPage).get();
             Elements elements = document.select(".js-card");
 
             for (Element element : elements) {
                 String image = "https://www.uploadvr.com" + element.select(".c-card__image").attr("data-src");//data-src
+
+//                if (checkNews(image)){
+//                    continue;
+//                }
+
                 String title = element.select(".c-card__headline").text();
                 String date = element.select(".c-timestamp").text();
                 String urlToNews = "https://www.uploadvr.com" + element.select(".c-card__headline a").attr("href");
-
-                events.add(new EventCreateDTO(title, image, urlToNews, date));
+                String[] arr = readText(urlToNews);
+                events.add(new EventCreateDTO(title, image,arr[0], arr[1], date));
             }
 
         } catch (IOException e) {
             log.error(e.getMessage());
         }
 
+        for(EventCreateDTO dto:events ){
+            Event event = new Event();
+            event.setTitle(dto.getTitle());
+            event.setText(dto.getText());
+            event.setImageUrl(dto.getImageUrl());
+//            event.setDateTime(); //todo add convert data time
+            eventRepository.save(event);
+        }
+
 //        return events;
-        System.out.println(events);
+        System.out.println();
+    }
+
+
+    private String[] readText(String urlToNews) throws IOException {
+        String[] str = new String[2];
+        Document document = Jsoup.connect(urlToNews).get();
+        String video = document.select(".fluid-width-video-wrapper iframe").attr("src");
+        Elements elements = document.select(".c-content p");
+//        String p = elements.html();
+        String p = elements.toString();
+
+        str[0] = video; // todo mast be make refactoring
+        str[1] = p;
+        return str;
+    }
+
+
+    private boolean checkNews(String imageUrl) {
+        return eventRepository.existsByImageUrl(imageUrl);
     }
 
 }
