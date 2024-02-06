d3678b520c3bf0e0600751c13a60723c29bedba1
punisher <semendiaievaviktoriia@gmail.com>
2024-02-06 14:38:48 +0100

    added data_vika_sql

--- src/main/resources/db/sql/data.sql
+++ src/main/resources/db/sql/data.sql
@@ -1,14 +1,3 @@
-INSERT INTO games (state,min_age,number_of_players,price,image, title, description, genre,release_date)
-values ('PENDING','EIGHT_PLUS','ONE_TWO',23,'https://portal-vr.ru/wp-content/uploads/2023/02/skgev4xstvwl9qdwux96p3-768x432.jpg'
-       ,'Resident Evil 4 VR','Исследуйте мир Resident Evil 4, полностью переработанный для виртуальной реальности. Попробуйте себя в роли специального агента Леона С. Кеннеди в его миссии по спасению дочери президента США, похищенной таинственным культом. Столкнитесь лицом к лицу с врагами и раскройте секреты игрового процесса, который произвел революцию в жанре ужасов на выживание. Сразитесь с ужасными существами и сразитесь с жителями деревни, контролируемыми разумом, и выясните их связь с культом, стоящим за похищением.'
-       ,'SHOOTER','2023-02-15'),
-       ('ACTIVE','SIXTEEN_PLUS','ONE_FOUR',21,'https://portal-vr.ru/wp-content/uploads/2023/07/portal-strike-kids_2.1-768x432.jpg','Portal Strike: Kids',
-        'Portal Strike: Kids проходит в зале VR Arena – большом открытом пространстве, где игроки передвигаются своими ногами. В Portal Strike: Kids представлены 5 проработанных карт; юные игроки побывают в Египте, Средневековье, на пиратском корабле и в киберпанке'
-           ,'SPORTS','2022-01-10'),
-       ('COMPLETED','EIGHTEEN_PLUS','ONE_TWO',13,'https://portal-vr.ru/wp-content/uploads/2023/10/arena-heroes-v2-9h16-768x432.jpg'
-       ,'Portal Arena Heroes','Portal Arena Heroes — это уникальная VR игра в стиле фентези на арене со свободным передвижением. Выберите 1 из 3-х классов и сражайтесь плечом к плечу на различных аренах команда против команды или совместно против армии монстров. Каждый класс имеет свои уникальные атаки и ультимативные способности. Окунитесь в настоящий мир фентези вместе с друзьями в клубах Portal VR!'
-       ,'ACTION','2023-02-15');
-
 INSERT INTO users (state, avatar, created_at, birth_date, nickname, firstname, lastname, phone, role, email, password)
 VALUES
     ('CONFIRMED', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAqAZG7zLGhZIxSUV6EVLfQX3WEUawmvM-eA&usqp=CAU', '2004-10-19 10:23:54',DATE '1990-01-10', 'Fishka' ,'Ivan', 'Ivanov', '+38096584402', 'ADMIN', 'ivan@gmail.com', '{noop}1Fishka1!'),
